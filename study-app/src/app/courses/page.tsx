78a768be4c155ece5eca0aabc58673a101dd6eb9
liamesika <liamesika2121@gmail.com>
2026-03-15 13:28:59 +0200

    Create page.tsx

---
+++ study-app/src/app/courses/page.tsx
@@ -0,0 +1,12 @@
+export default function CoursesPage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Courses</h1>
+      <ul>
+        <li><a href="/courses/calculus">Calculus</a></li>
+        <li><a href="/courses/discrete">Discrete</a></li>
+        <li><a href="/courses/linear-algebra">Linear Algebra</a></li>
+      </ul>
+    </main>
+  );
+}
